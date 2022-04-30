f52cb3ac1fba7edd2d7f8b0e3e3437f3433c27f7
uoa-ypec413 <ypec413@aucklanduni.ac.nz>
2022-04-30 17:20:25 +1200

    Add mantine UI and tailwind packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { Text } from '@mantine/core';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-const App = () => <BrowserRouter>Hello World!</BrowserRouter>;
+const App = () => (
+  <BrowserRouter>
+    <Text>Hello World!</Text>
+  </BrowserRouter>
+);
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { MantineProvider } from '@mantine/core';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider>
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
