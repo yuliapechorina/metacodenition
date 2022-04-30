c6548b847d15fd9e6b73c7d14b2b0fa64c25e15e
Keith Anderson <kand198@aucklanduni.ac.nz>
2022-04-30 21:31:22 +1200

    Set up Firebase, add auth demo (#4)

---
+++ src/firebase/authentication.ts
@@ -0,0 +1,25 @@
+/* eslint-disable no-console */
+/* eslint-disable no-alert */
+// TODO: Implement a better way of presenting messages to the user, to prevent the use of
+// console.log, alert, etc.
+import { Auth, AuthProvider, signInWithPopup } from "firebase/auth";
+import { doc, Firestore, setDoc } from "firebase/firestore/lite";
+
+const signIn = async (authenticator: Auth, provider: AuthProvider, database: Firestore ) => {
+    try {
+      const signInResult = await signInWithPopup(authenticator, provider);
+      const { user } = signInResult;
+      const id: string = user.uid;
+      await setDoc(doc(database, "users/", id), {
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: "google",
+        email: user.email,
+      });
+    } catch (error: any) {
+      console.error(error);
+      alert(error.message);
+    }
+};
+
+export default signIn;
