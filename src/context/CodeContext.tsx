49c539d128bc38af854c5d3793c2da5fe4fcf26f
uoa-ypec413 <ypec413@aucklanduni.ac.nz>
2022-06-02 16:02:42 +1200

    Persist files to localstorage

--- src/context/CodeContext.tsx
+++ src/context/CodeContext.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 type File = {
   id: number;
@@ -18,22 +18,36 @@ type CodeProviderProps = {
   children: React.ReactNode;
 };
 
-export const CodeProvider = ({ children }: CodeProviderProps) => {
-  const [files, setFiles] = useState<File[]>([
-    {
-      id: 0,
-      name: 'main.c',
-      content: '#include "main.h"\n\nint main() {\n  printf("Hello, World!");\n  return 0;\n}',
-    },
-    {
-      id: 1,
-      name: 'main.h',
-      content: '#include <stdio.h>\n\nint main();',
-    }
-  ]);
+const defaultFiles: File[] = [
+  {
+    id: 0,
+    name: 'main.c',
+    content:
+      '#include "main.h"\n\nint main() {\n  printf("Hello, World!");\n  return 0;\n}',
+  },
+  {
+    id: 1,
+    name: 'main.h',
+    content: '#include <stdio.h>\n\nint main();',
+  },
+];
 
-  const editFile = (fileId: number, content?: string) => {
-    files!.find((f) => f.id === fileId)!.content = content!;
+const initialFiles: File[] =
+  JSON.parse(localStorage.getItem('files') as string) || defaultFiles;
+
+export const CodeProvider = ({ children }: CodeProviderProps) => {
+  const [files, setFiles] = useState<File[]>(initialFiles);
+
+  const editFile = (fileId: number, content: string) => {
+    const newFiles = files.map((file) =>
+      file.id === fileId
+        ? {
+            ...file,
+            content,
+          }
+        : file
+    );
+    setFiles(newFiles);
   };
 
   const context = React.useMemo(
@@ -44,6 +58,10 @@ export const CodeProvider = ({ children }: CodeProviderProps) => {
     }),
     [files, setFiles, editFile]
   );
+
+  useEffect(() => {
+    localStorage.setItem('files', JSON.stringify(files));
+  }, [files]);
 
   return (
     <CodeContext.Provider value={context}>{children}</CodeContext.Provider>
