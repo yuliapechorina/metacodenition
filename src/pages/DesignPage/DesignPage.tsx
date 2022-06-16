06f5d5f0b332f238dff886c84d994216467ca096
uoa-ypec413 <ypec413@aucklanduni.ac.nz>
2022-06-16 14:01:23 +1200

    Add describe action section

--- src/pages/DesignPage/DesignPage.tsx
+++ src/pages/DesignPage/DesignPage.tsx
@@ -1,19 +1,32 @@
 import {
+  Button,
   Divider,
+  Group,
+  Input,
   Stack,
   Text,
   Title,
   TypographyStylesProvider,
 } from '@mantine/core';
 import HTMLReactParser from 'html-react-parser';
-import React from 'react';
+import React, { useState } from 'react';
 import useProblem from '../../context/ProblemContext';
 
 const DesignPage = () => {
   const { problemStatement, highlightProblemChunk } = useProblem();
+  const [highlightedChunk, setHighlightedChunk] = useState<string | undefined>(
+    undefined
+  );
+  const [inputValue, setInputValue] = useState<string | undefined>(undefined);
 
   const handleMouseUp = () => {
+    console.log(`Highlighted:  ${window.getSelection()?.toString()!}`);
+    setHighlightedChunk(window.getSelection()?.toString()!);
     highlightProblemChunk!(window.getSelection()?.toString()!);
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.currentTarget.value);
   };
 
   return (
@@ -29,6 +42,34 @@ const DesignPage = () => {
       </Text>
       <Divider />
       <Title order={4}>Describe an action:</Title>
+      {highlightedChunk === undefined ? (
+        <Text>Nothing highlighted yet!</Text>
+      ) : (
+        <Text>
+          Highlighted text:{' '}
+          <Text inherit component='span' className=' font-bold'>
+            {highlightedChunk}
+          </Text>
+        </Text>
+      )}
+      <Group className='w-full'>
+        <Input
+          size='md'
+          className='grow'
+          classNames={{
+            input: 'font-mono rounded bg-gray-100 w-full',
+          }}
+          placeholder='Describe an action here...'
+          onChange={handleInputChange}
+          value={inputValue}
+        />
+        <Button
+          size='md'
+          className='bg-emerald-500 fill-emerald-50 hover:bg-emerald-600'
+        >
+          Submit
+        </Button>
+      </Group>
     </Stack>
   );
 };
