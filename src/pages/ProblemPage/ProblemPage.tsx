14cb8b156894c7e8234be2aceab56383447c9293
uoa-ypec413 <ypec413@aucklanduni.ac.nz>
2022-06-16 12:12:43 +1200

    Add text highlighting

--- src/pages/ProblemPage/ProblemPage.tsx
+++ src/pages/ProblemPage/ProblemPage.tsx
@@ -6,18 +6,34 @@ import {
   TypographyStylesProvider,
 } from '@mantine/core';
 import HTMLReactParser from 'html-react-parser';
-import React from 'react';
+import React, { useState } from 'react';
 import useProblem from '../../context/ProblemContext';
 
 const ProblemPage = () => {
   const { problemStatement } = useProblem();
+  const [problem, setProblem] = useState(problemStatement);
+
+  const highlightChunk = (chunk: string) => {
+    const highlightedProblem = problem!.replace(
+      chunk,
+      `<mark className='bg-yellow-200'>${chunk}</mark>`
+    );
+    setProblem(highlightedProblem);
+  };
+
+  const handleMouseUp = () => {
+    highlightChunk(window.getSelection()?.toString()!);
+  };
 
   return (
     <Stack className='p-2'>
       <Title order={4}>Problem:</Title>
       <Text className='text-justify'>
-        <TypographyStylesProvider>
-          {HTMLReactParser(problemStatement!)}
+        <TypographyStylesProvider
+          onMouseUp={handleMouseUp}
+          className='selection:bg-yellow-200'
+        >
+          {HTMLReactParser(problem!)}
         </TypographyStylesProvider>
       </Text>
       <Divider />
