3c2eaf236c478badd5eecf0270df86f6841f3aa4
uoa-ypec413 <ypec413@aucklanduni.ac.nz>
2022-06-30 15:44:12 +1200

    Add code running api

---
+++ src/api/CodeRunnerAPI.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+export interface IRunSpec {
+  language_id: string;
+  sourcefilename: string;
+  sourcecode: string;
+}
+
+export interface IRunParameters {
+  run_spec: IRunSpec;
+}
+
+export const submitRun = async (
+  runParameters: IRunParameters
+): Promise<any> => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json; charset-utf-8',
+    },
+  };
+  const response = await axios.put(
+    'https://code-runner-oxrju5up5q-ts.a.run.app/jobe/index.php/restapi/runs',
+    runParameters,
+    config
+  );
+  console.log('SUBMIT RUN RESPONSE', response);
+  console.log('SUBMIT RUN RESPONSE DATA', response.data);
+  return response.data;
+};
--- src/components/CodeRunArea/CodeRunArea.tsx
+++ src/components/CodeRunArea/CodeRunArea.tsx
@@ -1,5 +1,7 @@
 import { Stack } from '@mantine/core';
 import { useState } from 'react';
+import { submitRun } from '../../api/CodeRunnerAPI';
+import useCode from '../../context/CodeContext';
 import InputArea from './InputArea';
 import OutputArea from './OutputArea';
 
@@ -10,8 +12,24 @@ type CodeRunAreaProps = {
 const CodeRunArea = (props: CodeRunAreaProps) => {
   const { className } = props;
   const [loading, setLoading] = useState<boolean>(false);
+  const { file } = useCode();
+
   const run = () => {
     setLoading(true);
+    const runCode = async () => {
+      try {
+        await submitRun({
+          run_spec: {
+            language_id: 'c',
+            sourcefilename: 'test.c',
+            sourcecode: file!.content,
+          },
+        });
+      } catch (error: any) {
+        console.log(error);
+      }
+    };
+    runCode();
     setTimeout(setLoading, 1000, false);
   };
   return (
