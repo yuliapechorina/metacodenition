2d138fbe2121228c21ea10401f22fb1801f88f76
uoa-ypec413 <ypec413@aucklanduni.ac.nz>
2022-08-08 19:49:33 +1200

    Implement submission modal

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,37 +1,38 @@
 import { Navbar } from '@mantine/core';
-import { useNavigate } from 'react-router-dom';
-import useAssignment from '../../context/AssignmentContext';
-import useCode from '../../context/CodeContext';
+import { useState } from 'react';
 import useQuestion from '../../hooks/useQuestion';
 import GenericButton from '../generics/GenericButton';
 import MainLinks from '../MainLinks';
+import SubmissionModal from '../SubmissionModal';
 
 const NavBar = () => {
-  const { setNextQuestion } = useAssignment();
-  const { isLoading, updateUserQuestionDocument } = useQuestion();
-  const navigate = useNavigate();
-  const { file } = useCode();
+  const { isLoading } = useQuestion();
+  const [submissionModalOpened, setSubmissionModalOpened] = useState(false);
 
-  const handleSubmitButtonPress = async () => {
-    updateUserQuestionDocument({ submitted: true, userCode: file });
-    setNextQuestion!();
-    navigate('/assignment');
+  const openSubmissionModal = () => {
+    setSubmissionModalOpened(true);
   };
 
   return (
-    <Navbar className='p-1 w-fit'>
-      <Navbar.Section>
-        <MainLinks />
-      </Navbar.Section>
-      <Navbar.Section className='justify-self-end mt-auto pb-4 mx-auto'>
-        <GenericButton
-          text='Submit'
-          onClick={handleSubmitButtonPress}
-          disabled={isLoading}
-          loading={isLoading}
-        />
-      </Navbar.Section>
-    </Navbar>
+    <>
+      <Navbar className='p-1 w-fit'>
+        <Navbar.Section>
+          <MainLinks />
+        </Navbar.Section>
+        <Navbar.Section className='justify-self-end mt-auto pb-4 mx-auto'>
+          <GenericButton
+            text='Submit'
+            onClick={openSubmissionModal}
+            disabled={isLoading}
+            loading={isLoading}
+          />
+        </Navbar.Section>
+      </Navbar>
+      <SubmissionModal
+        opened={submissionModalOpened}
+        setOpened={setSubmissionModalOpened}
+      />
+    </>
   );
 };
 
---
+++ src/components/SubmissionModal/SubmissionModal.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Group, Modal, Text } from '@mantine/core';
+import { useNavigate } from 'react-router-dom';
+import useAssignment from '../../context/AssignmentContext';
+import GenericButton from '../generics/GenericButton';
+import useQuestion from '../../hooks/useQuestion';
+import useCode from '../../context/CodeContext';
+
+type SubmissionModalProps = {
+  opened: boolean;
+  setOpened: (value: React.SetStateAction<boolean>) => void;
+};
+const SubmissionModal = ({ opened, setOpened }: SubmissionModalProps) => {
+  const { questionNumber, setNextQuestion } = useAssignment();
+  const { isLoading, updateUserQuestionDocument } = useQuestion();
+  const { file } = useCode();
+  const navigate = useNavigate();
+
+  const handleClickYes = () => {
+    updateUserQuestionDocument({ submitted: true, userCode: file });
+    setNextQuestion!();
+    navigate('/assignment');
+    setOpened(false);
+  };
+
+  const handleClickNo = () => {
+    setOpened(false);
+  };
+
+  return (
+    <Modal
+      centered
+      opened={opened}
+      withCloseButton={false}
+      onClose={() => setOpened(false)}
+      title={`You're about to submit Question ${questionNumber}. Are you sure?`}
+      classNames={{
+        title: 'font-bold',
+        body: 'space-y-8',
+      }}
+    >
+      <Text className='text-sm'>
+        You won&apos;t be able to go back and edit your answer once you submit.
+      </Text>
+      <Group className='space-x-1 justify-center'>
+        <GenericButton
+          text='Yes'
+          onClick={handleClickYes}
+          disabled={isLoading}
+          loading={isLoading}
+        />
+        <GenericButton
+          text='No'
+          red
+          onClick={handleClickNo}
+          disabled={isLoading}
+          loading={isLoading}
+        />
+      </Group>
+    </Modal>
+  );
+};
+
+export default SubmissionModal;
