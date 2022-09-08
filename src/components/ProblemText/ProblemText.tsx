a10e9c14dfde6b735983378f1f4e02105ceae4da
Keith Anderson <68878136+kand198@users.noreply.github.com>
2022-09-09 11:26:04 +1200

    🛑 Stop highlight tooltips from overflowing (#92)
    * Stop highlight tooltips from overflowing
    
    * Update src/components/ProblemText/ProblemText.tsx
    
    Co-authored-by: Yulia Pechorina <68877945+uoa-ypec413@users.noreply.github.com>
    
    Co-authored-by: Yulia Pechorina <68877945+uoa-ypec413@users.noreply.github.com>

--- src/components/ProblemText/ProblemText.tsx
+++ src/components/ProblemText/ProblemText.tsx
@@ -23,23 +23,35 @@ const ProblemText = ({
     document.querySelectorAll<HTMLElement>('.tooltiptext')
   );
 
+  const textRef = React.useRef<HTMLDivElement>(null);
+
   const stickyTooltip = (e: React.MouseEvent) => {
     const relativeParent = relativeParentId
       ? document.getElementById(relativeParentId)
       : undefined;
+    const boundingRect = relativeParent
+      ? relativeParent?.getBoundingClientRect()
+      : textRef.current?.getBoundingClientRect();
+    const parentTop = boundingRect?.top ?? 0;
+    const parentLeft = boundingRect?.left ?? 0;
     tooltip.forEach((t) => {
+      // Calculate the left offset based on whether the tooltip is inside a relative parent
+      const maxWidth =
+        e.clientX - parentLeft + t.offsetWidth / 2 - (boundingRect?.width ?? 0);
+      const minWidth = e.clientX - parentLeft - t.offsetWidth / 2;
+      const leftOffset = maxWidth > 0 ? maxWidth : 0;
+      const rightOffset = minWidth < 0 ? minWidth : 0;
+      const xOffset =
+        t.offsetWidth / 2 +
+        leftOffset +
+        rightOffset +
+        (relativeParent ? parentLeft : 0);
+      const yOffset = (tooltipOffset ?? 50) + (relativeParent ? parentTop : 0);
+
       // eslint-disable-next-line no-param-reassign
-      t.style.top = `${
-        e.clientY -
-        (relativeParent?.getBoundingClientRect().top ?? 0) -
-        (tooltipOffset ?? 50)
-      }px`;
+      t.style.top = `${e.clientY - yOffset}px`;
       // eslint-disable-next-line no-param-reassign
-      t.style.left = `${
-        e.clientX -
-        (relativeParent?.getBoundingClientRect().left ?? 0) -
-        t.offsetWidth / 2
-      }px`;
+      t.style.left = `${e.clientX - xOffset}px`;
     });
   };
 
@@ -52,6 +64,7 @@ const ProblemText = ({
       className={`${className} ${textClassNames} text-justify`}
       onMouseUp={onMouseUp}
       onMouseMove={(e: React.MouseEvent) => stickyTooltip(e)}
+      ref={textRef}
     >
       <TypographyStylesProvider className={providerClassNames}>
         {HTMLReactParser(getProblemStatement!())}
