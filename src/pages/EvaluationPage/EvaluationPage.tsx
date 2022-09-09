e37cef76f6df14dc7176e58ded4b2253c05422a1
Keith Anderson <kand198@aucklanduni.ac.nz>
2022-09-09 21:54:46 +1200

    Fix hover background colour

--- src/pages/EvaluationPage/EvaluationPage.tsx
+++ src/pages/EvaluationPage/EvaluationPage.tsx
@@ -253,7 +253,7 @@ const EvaluationPage = () => {
                   <Center>
                     <UnstyledButton
                       onClick={() => handleAddAction()}
-                      className='hover:bg-gray-100 inline-flex items-center p-3 text-sm space-x-1 rounded-md'
+                      className='hover:bg-gray-200 inline-flex items-center p-3 text-sm space-x-1 rounded-md'
                     >
                       <Text size='sm'>Add a new action</Text>
                       <HiPlus />
