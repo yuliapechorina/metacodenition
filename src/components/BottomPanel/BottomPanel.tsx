aa0d8893de1f49ce44a89a2d6aa2f73222019c78
Yulia Pechorina <68877945+uoa-ypec413@users.noreply.github.com>
2022-09-13 17:09:58 +1200

    :rocket: Add ability to disable action plan tab (#100)

--- src/components/BottomPanel/BottomPanel.tsx
+++ src/components/BottomPanel/BottomPanel.tsx
@@ -1,23 +1,35 @@
 import { Stack, Tabs } from '@mantine/core';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { HiOutlinePlay } from 'react-icons/hi';
 import { IoRocketOutline } from 'react-icons/io5';
+import useInterventions from '../../hooks/useInterventions';
 import ActionPlan from '../ActionPlan';
 import CodeRunArea from '../CodeRunArea';
 
 const BottomPanel = () => {
   const [currentTabIndex, setCurrentTabIndex] = React.useState(0);
+  const { interventions } = useInterventions();
+  const [actionPlanTabEnabled, setActionPlanTabEnabled] = useState(true);
+
+  useEffect(() => {
+    const evaluatingASolutionEnabled = interventions.find(
+      ({ name }) => name === 'Evaluating a solution'
+    )?.enabled;
+    setActionPlanTabEnabled(evaluatingASolutionEnabled ?? false);
+  }, [interventions]);
 
   return (
     <Stack className='p-4 pt-0 relative'>
       <Tabs active={currentTabIndex} onTabChange={setCurrentTabIndex}>
-        <Tabs.Tab
-          label='Action Plan'
-          key={0}
-          icon={<IoRocketOutline size={20} />}
-        >
-          <ActionPlan />
-        </Tabs.Tab>
+        {actionPlanTabEnabled && (
+          <Tabs.Tab
+            label='Action Plan'
+            key={0}
+            icon={<IoRocketOutline size={20} />}
+          >
+            <ActionPlan />
+          </Tabs.Tab>
+        )}
         <Tabs.Tab label='Run Code' key={1} icon={<HiOutlinePlay size={20} />}>
           <CodeRunArea />
         </Tabs.Tab>
