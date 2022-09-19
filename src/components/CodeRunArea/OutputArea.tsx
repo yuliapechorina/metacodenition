a9b146832d7cbf0a7cd3d239596fa44513735dd5
Yulia Pechorina <68877945+uoa-ypec413@users.noreply.github.com>
2022-09-19 19:15:12 +1200

    :fire: Fix hidden output in implementation page (#110)

--- src/components/CodeRunArea/OutputArea.tsx
+++ src/components/CodeRunArea/OutputArea.tsx
@@ -9,10 +9,10 @@ const OutputArea = (props: OutputAreaProps) => {
   const { loading, text } = props;
 
   return (
-    <Stack className='h-full'>
+    <Stack className='h-full pb-10'>
       <LoadingOverlay visible={loading} />
       <Text className='text-lg font-bold'>Output</Text>
-      <Code className='text-base min-h-20 bg-gray-100 p-4 rounded-md fit-content whitespace-pre'>
+      <Code className='min-h-20 bg-gray-100 p-4 rounded-md w-full whitespace-pre-wrap text-sm'>
         {text}
       </Code>
     </Stack>
