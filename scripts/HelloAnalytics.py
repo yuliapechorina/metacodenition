6e4cc94b51960acbfc534f9633c96d8bd06d4f21
uoa-ypec413 <ypec413@aucklanduni.ac.nz>
2022-09-23 19:25:02 +1200

    Add check test case report

--- scripts/HelloAnalytics.py
+++ scripts/HelloAnalytics.py
@@ -2,7 +2,6 @@ from google.analytics.data_v1beta import BetaAnalyticsDataClient
 from google.analytics.data_v1beta.types import *
 from google.oauth2 import service_account
 
-
 def sample_run_report(client, property_id):
     request = RunReportRequest(
         property=f"properties/{property_id}",
@@ -87,19 +86,22 @@ def run_test_cases_report(client: BetaAnalyticsDataClient, property_id):
             )
         ),
   )
-  response: RunResponse = client.run_report(request)
+  response: RunReportResponse = client.run_report(request)
   
   print("Writing run_test_cases events to file");
   # Print to file
   with open("test_cases_report.txt", "w") as f:
     for dimension_header in response.dimension_headers:
+      dimension_header.name = dimension_header.name.replace("customEvent:", "")
       f.write(f"{dimension_header.name}\t")
     for metric_header in response.metric_headers:
+      metric_header.name = metric_header.name.replace("customEvent:", "")
       f.write(f"{metric_header.name}\t")
     f.write("\n")
 
     # Sort rows by dimension customEvent:user_upi
     sorted_rows = sorted(response.rows, key=lambda row: row.dimension_values[0].value)
+
     for row in sorted_rows:
       for dimension in row.dimension_values:
         f.write(f"{dimension.value}\t")
@@ -109,11 +111,83 @@ def run_test_cases_report(client: BetaAnalyticsDataClient, property_id):
 
   print("Added run_test_cases events to file")
 
+def check_test_case_report(client: BetaAnalyticsDataClient, property_id):
+  request = RunReportRequest(
+    property=f"properties/{property_id}",
+    dimensions=[Dimension(name="customEvent:user_upi"), Dimension(name="customEvent:user_testing_group"), Dimension(name="customEvent:question_number"), Dimension(name="customEvent:current_test_case")],
+    metrics=[Metric(name="eventCount")],
+    date_ranges=[DateRange(start_date="2022-09-18", end_date="today")],
+    dimension_filter=FilterExpression(
+            and_group=FilterExpressionList(
+                expressions=[
+                    FilterExpression(
+                          not_expression=FilterExpression(
+                          filter=Filter(
+                              field_name="customEvent:current_test_case",
+                              string_filter=Filter.StringFilter(value="(not set)"),
+                          )
+                        )
+                      ),
+                    FilterExpression(
+                      not_expression=FilterExpression(
+                        filter=Filter(
+                            field_name="customEvent:user_upi",
+                            string_filter=Filter.StringFilter(value="(not set)"),
+                        )
+                      )
+                    ),
+                    FilterExpression(
+                        not_expression=FilterExpression(
+                        filter=Filter(
+                            field_name="customEvent:user_upi",
+                            string_filter=Filter.StringFilter(value="ypec413"),
+                        )
+                      )
+                    ),
+                    FilterExpression(
+                        not_expression=FilterExpression(
+                        filter=Filter(
+                            field_name="customEvent:user_upi",
+                            string_filter=Filter.StringFilter(value="kand198"),
+                        )
+                      )
+                    ),
+                ]
+            )
+        ),
+  )
+  response: RunResponse = client.run_report(request)
+  
+  print("Writing check_test_case events to file");
+  # Print to file
+  with open("check_test_case_report.txt", "w") as f:
+    for dimension_header in response.dimension_headers:
+      dimension_header.name = dimension_header.name.replace("customEvent:", "")
+      f.write(f"{dimension_header.name}\t")
+    for metric_header in response.metric_headers:
+      metric_header.name = metric_header.name.replace("customEvent:", "")
+      f.write(f"{metric_header.name}\t")
+    f.write("\n")
+
+    # Sort rows by dimension customEvent:user_upi
+    sorted_rows = sorted(response.rows, key=lambda row: row.dimension_values[0].value)
+
+    for row in sorted_rows:
+      for dimension in row.dimension_values:
+        f.write(f"{dimension.value}\t")
+      for metric in row.metric_values:
+        f.write(f"{metric.value}\t")
+      f.write("\n")
+
+  print("Added check_test_case events to file")
+  
+
 def main():
   credentials = service_account.Credentials.from_service_account_file(".\pedagogic-ide-a137fcb189d5.json")
   client = BetaAnalyticsDataClient(credentials=credentials)
   # sample_run_report(client, "313384509")
   # sample_run_relatime_report(client, "313384509")
-  run_test_cases_report(client, "313384509")
+  # run_test_cases_report(client, "313384509")
+  check_test_case_report(client, "313384509")
 if __name__ == '__main__':
   main()
