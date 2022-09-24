b8e0bcc7028610b7d55b0ded1aca8e98910f82b4
uoa-ypec413 <ypec413@aucklanduni.ac.nz>
2022-09-24 14:11:32 +1200

    Add more reports

--- scripts/HelloAnalytics.py
+++ scripts/HelloAnalytics.py
@@ -1,193 +1,111 @@
 from google.analytics.data_v1beta import BetaAnalyticsDataClient
 from google.analytics.data_v1beta.types import *
 from google.oauth2 import service_account
-
-def sample_run_report(client, property_id):
-    request = RunReportRequest(
-        property=f"properties/{property_id}",
-        dimensions=[Dimension(name="customUser:user_group")],
-        metrics=[Metric(name="activeUsers")],
-        date_ranges=[DateRange(start_date="2022-09-18", end_date="today")],
-    )
-    response = client.run_report(request)
-
-    print("Report result:")
-    for dimension_header in response.dimension_headers:
-      print(dimension_header.name, end="\t")
-  
-    for metric_header in response.metric_headers:
-      print(metric_header.name, end="\n")
-    
-    for row in response.rows:
-      rowString = ''
-      for dimension in row.dimension_values:
-        rowString += f"{dimension.value}\t\t"
-      for metric in row.metric_values:
-        rowString += f"{metric.value}\t\t"
-      print(rowString)
-
-def sample_run_relatime_report(client: BetaAnalyticsDataClient, property_id):
-  request = RunRealtimeReportRequest(
-    property=f"properties/{property_id}",
-    dimensions=[Dimension(name="customUser:user_group")],
-    metrics=[Metric(name="activeUsers")],
-  )
-  response: RunRealtimeReportResponse = client.run_realtime_report(request)
-
-  print("Realtime report result:")
-  for dimension_header in response.dimension_headers:
-    print(dimension_header.name, end="\t")
-  
-  for metric_header in response.metric_headers:
-    print(metric_header.name, end="\t\n")
-  
-  for row in response.rows:
-    rowString = ''
-    for dimension in row.dimension_values:
-      rowString += f"{dimension.value}\t"
-    for metric in row.metric_values:
-      rowString += f"{metric.value}\t"
-    print(rowString)
+from run_report_utils import *
 
 def run_test_cases_report(client: BetaAnalyticsDataClient, property_id):
-  request = RunReportRequest(
-    property=f"properties/{property_id}",
+  response: RunReportResponse = run_report(
+    client,
+    property_id,
     dimensions=[Dimension(name="customEvent:user_upi"), Dimension(name="customEvent:user_testing_group"), Dimension(name="customEvent:question_number")],
     metrics=[Metric(name="customEvent:num_passed"), Metric(name="customEvent:num_test_cases")],
-    date_ranges=[DateRange(start_date="2022-09-18", end_date="today")],
-    dimension_filter=FilterExpression(
-            and_group=FilterExpressionList(
-                expressions=[
-                    FilterExpression(
-                      not_expression=FilterExpression(
-                        filter=Filter(
-                            field_name="customEvent:user_upi",
-                            string_filter=Filter.StringFilter(value="(not set)"),
-                        )
-                      )
-                    ),
-                    FilterExpression(
-                        not_expression=FilterExpression(
-                        filter=Filter(
-                            field_name="customEvent:user_upi",
-                            string_filter=Filter.StringFilter(value="ypec413"),
-                        )
-                      )
-                    ),
-                    FilterExpression(
-                        not_expression=FilterExpression(
-                        filter=Filter(
-                            field_name="customEvent:user_upi",
-                            string_filter=Filter.StringFilter(value="kand198"),
-                        )
-                      )
-                    ),
-                ]
-            )
-        ),
-  )
-  response: RunReportResponse = client.run_report(request)
-  
-  print("Writing run_test_cases events to file");
-  # Print to file
-  with open("test_cases_report.txt", "w") as f:
-    for dimension_header in response.dimension_headers:
-      dimension_header.name = dimension_header.name.replace("customEvent:", "")
-      f.write(f"{dimension_header.name}\t")
-    for metric_header in response.metric_headers:
-      metric_header.name = metric_header.name.replace("customEvent:", "")
-      f.write(f"{metric_header.name}\t")
-    f.write("\n")
-
-    # Sort rows by dimension customEvent:user_upi
-    sorted_rows = sorted(response.rows, key=lambda row: row.dimension_values[0].value)
-
-    for row in sorted_rows:
-      for dimension in row.dimension_values:
-        f.write(f"{dimension.value}\t")
-      for metric in row.metric_values:
-        f.write(f"{metric.value}\t")
-      f.write("\n")
-
-  print("Added run_test_cases events to file")
+  );
+  write_report_to_file(response, "test_cases_report.txt")
 
 def check_test_case_report(client: BetaAnalyticsDataClient, property_id):
-  request = RunReportRequest(
-    property=f"properties/{property_id}",
-    dimensions=[Dimension(name="customEvent:user_upi"), Dimension(name="customEvent:user_testing_group"), Dimension(name="customEvent:question_number"), Dimension(name="customEvent:current_test_case")],
+  dimension_filters = [
+      FilterExpression(
+      not_expression=FilterExpression(
+        filter=Filter(
+          field_name="customEvent:current_test_case",
+          string_filter=Filter.StringFilter(value="(not set)"),
+        )
+      )
+    )
+  ]
+  response: RunReportResponse = run_report(
+    client,
+    property_id,
+    dimensions=[Dimension(name="customEvent:user_upi"), Dimension(name="customEvent:user_testing_group"), 
+                Dimension(name="customEvent:question_number"), Dimension(name="customEvent:current_test_case")],
     metrics=[Metric(name="eventCount")],
-    date_ranges=[DateRange(start_date="2022-09-18", end_date="today")],
-    dimension_filter=FilterExpression(
-            and_group=FilterExpressionList(
-                expressions=[
-                    FilterExpression(
-                          not_expression=FilterExpression(
-                          filter=Filter(
-                              field_name="customEvent:current_test_case",
-                              string_filter=Filter.StringFilter(value="(not set)"),
-                          )
-                        )
-                      ),
-                    FilterExpression(
-                      not_expression=FilterExpression(
-                        filter=Filter(
-                            field_name="customEvent:user_upi",
-                            string_filter=Filter.StringFilter(value="(not set)"),
-                        )
-                      )
-                    ),
-                    FilterExpression(
-                        not_expression=FilterExpression(
-                        filter=Filter(
-                            field_name="customEvent:user_upi",
-                            string_filter=Filter.StringFilter(value="ypec413"),
-                        )
-                      )
-                    ),
-                    FilterExpression(
-                        not_expression=FilterExpression(
-                        filter=Filter(
-                            field_name="customEvent:user_upi",
-                            string_filter=Filter.StringFilter(value="kand198"),
-                        )
-                      )
-                    ),
-                ]
-            )
-        ),
-  )
-  response: RunResponse = client.run_report(request)
+    dimension_filters=dimension_filters,
+  );
+  write_report_to_file(response, "check_test_case_report.txt")
+
+def submit_question_3_report (client: BetaAnalyticsDataClient, property_id):
+  dimension_filters = [
+    FilterExpression(
+      filter=Filter(
+        field_name="eventName",
+        string_filter=Filter.StringFilter(value="confirm_submit_question"),
+      )
+    )
+  ]
+  order_bys=[
+    OrderBy(
+    dimension=OrderBy.DimensionOrderBy(dimension_name="dateHour", order_type=OrderBy.DimensionOrderBy.OrderType.NUMERIC), 
+    desc=True
+    )
+  ]
+  response: RunReportResponse = run_report(
+    client,
+    property_id,
+    dimensions=[Dimension(name="customEvent:user_upi"), Dimension(name="customEvent:question_number"), Dimension(name="eventName"), Dimension(name="dateHour")],
+    metrics=[Metric(name="eventCount")],
+    dimension_filters=dimension_filters,
+    order_bys=order_bys,
+  );
   
-  print("Writing check_test_case events to file");
-  # Print to file
-  with open("check_test_case_report.txt", "w") as f:
-    for dimension_header in response.dimension_headers:
-      dimension_header.name = dimension_header.name.replace("customEvent:", "")
-      f.write(f"{dimension_header.name}\t")
-    for metric_header in response.metric_headers:
-      metric_header.name = metric_header.name.replace("customEvent:", "")
-      f.write(f"{metric_header.name}\t")
+  print(f"Writing to submit_question_3_report.txt");
+  with open("submit_question_3_report.txt", "w") as f:
+    upi_dimension_header_name = response.dimension_headers[0].name.replace("customEvent:", "")
+    f.write(f"{upi_dimension_header_name}\t")
+    date_hour_dimension_header_name = response.dimension_headers[3].name
+    f.write(f"{date_hour_dimension_header_name}\t")
     f.write("\n")
 
-    # Sort rows by dimension customEvent:user_upi
-    sorted_rows = sorted(response.rows, key=lambda row: row.dimension_values[0].value)
-
-    for row in sorted_rows:
-      for dimension in row.dimension_values:
-        f.write(f"{dimension.value}\t")
-      for metric in row.metric_values:
-        f.write(f"{metric.value}\t")
+    for row in response.rows:
+      upi_dimension_value = row.dimension_values[0].value
+      f.write(f"{upi_dimension_value}\t")
+      date_hour_dimension_value = row.dimension_values[3].value
+      f.write(f"{date_hour_dimension_value}\t")
       f.write("\n")
+  print("Added to submit_question_3_report.txt");
 
-  print("Added check_test_case events to file")
-  
+def select_interventions_report (client: BetaAnalyticsDataClient, property_id):
+  dimension_filters = [
+    FilterExpression(
+      filter=Filter(
+            field_name="eventName",
+            string_filter=Filter.StringFilter(value="select_interventions"),
+        )
+    ),
+    FilterExpression(
+      filter=Filter(
+            field_name="customEvent:question_number",
+            string_filter=Filter.StringFilter(value="3"),
+        )
+    ),
+  ]
+  response: RunReportResponse = run_report(
+    client,
+    property_id,
+    dimensions=[Dimension(name="customEvent:user_upi"), Dimension(name="customEvent:user_testing_group"), Dimension(name="eventName"), 
+                Dimension(name="customEvent:understanding_the_problem"), Dimension(name="customEvent:designing_a_solution"), 
+                Dimension(name="customEvent:evaluating_a_solution"), Dimension(name="customEvent:evaluating_implemented_solution"),
+                ],
+    metrics=[Metric(name="eventCount")],
+    dimension_filters=dimension_filters,
+  );
+  write_report_to_file(response, "select_interventions_report.txt")
 
 def main():
   credentials = service_account.Credentials.from_service_account_file(".\pedagogic-ide-a137fcb189d5.json")
   client = BetaAnalyticsDataClient(credentials=credentials)
-  # sample_run_report(client, "313384509")
-  # sample_run_relatime_report(client, "313384509")
   # run_test_cases_report(client, "313384509")
-  check_test_case_report(client, "313384509")
+  # check_test_case_report(client, "313384509")
+  submit_question_3_report(client, "313384509")
+  # select_interventions_report(client, "313384509")
 if __name__ == '__main__':
   main()
